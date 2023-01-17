3d07fc34530606d384f5fd635bff22fb8eb139d4
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2023-01-17 21:41:51 +0530

    i am back

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import styles from "./signup.module.css"
-import reg from '../../images/r.jpg'
+import styles from "./signup.module.css";
+import reg from "../../images/r.jpg";
+import { NavLink } from "react-router-dom";
 const Signup = () => {
   return (
     <>
@@ -21,70 +22,87 @@ const Signup = () => {
               />
             </div>
             <div className={styles.inp}>
-              <label htmlFor="name">
-                <i className="zmdi zmdi-account materials-icon-name"></i>
+              <label htmlFor="email">
+                <i className="zmdi zmdi-email materials-icon-name"></i>
               </label>
               <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your Name"
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Your Email"
                 autoComplete="off"
               />
             </div>
             <div className={styles.inp}>
-              <label htmlFor="name">
-                <i className="zmdi zmdi-account materials-icon-name"></i>
+              <label htmlFor="phone">
+                <i className="zmdi zmdi-phone-in-talk materials-icon-name"></i>
               </label>
               <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your Name"
+                type="number"
+                name="phone"
+                id="phone"
+                placeholder="Your Phone"
                 autoComplete="off"
               />
             </div>
             <div className={styles.inp}>
-              <label htmlFor="name">
-                <i className="zmdi zmdi-account materials-icon-name"></i>
+              <label htmlFor="work">
+                <i className="zmdi zmdi-slideshow materials-icon-name"></i>
               </label>
               <input
                 type="text"
-                name="name"
-                id="name"
-                placeholder="Your Name"
+                name="work"
+                id="work"
+                placeholder="Your Profession"
                 autoComplete="off"
               />
             </div>
             <div className={styles.inp}>
-              <label htmlFor="name">
-                <i className="zmdi zmdi-account materials-icon-name"></i>
+              <label htmlFor="password">
+                <i className="zmdi zmdi-lock materials-icon-name"></i>
               </label>
               <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your Name"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Your Password"
                 autoComplete="off"
               />
             </div>
             <div className={styles.inp}>
-              <label htmlFor="name">
-                <i className="zmdi zmdi-account materials-icon-name"></i>
+              <label htmlFor="cpassword">
+                <i className="zmdi zmdi-lock materials-icon-name"></i>
               </label>
               <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your Name"
+                type="password"
+                name="cpassword"
+                id="cpassword"
+                placeholder="Your Confirm Password"
                 autoComplete="off"
               />
             </div>
-            
+            <div>
+              <input
+                type="submit"
+                name="signup"
+                id="signup"
+                className={styles.formsubmit}
+                value={"Register"}
+              />
+            </div>
           </form>
         </div>
         <div className={styles.imgContainer}>
-          <img src={reg} width="100%" height={"250px"} />
+          <figure>
+        
+            <img
+              src={reg}
+              alt="registration pic"
+              width="100%"
+              height={"250px"}
+            />
+          </figure>
+          <NavLink to="/login">I am already registerd</NavLink>
         </div>
       </div>
     </>
